c2e9fc4ef74cc399a74d4b093674c004e8ef22e1
greatchriz <great.chriz@gmail.com>
2022-10-02 13:56:48 +0100

    Model and files generation

---
+++ app/Http/Controllers/SavingController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Saving;
+use App\Http\Requests\StoreSavingRequest;
+use App\Http\Requests\UpdateSavingRequest;
+
+class SavingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSavingRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSavingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Saving  $saving
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Saving $saving)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Saving  $saving
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Saving $saving)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSavingRequest  $request
+     * @param  \App\Models\Saving  $saving
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSavingRequest $request, Saving $saving)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Saving  $saving
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Saving $saving)
+    {
+        //
+    }
+}
